a9a6e6742c5f029536be3984dfd9e3eb4537df56
Koua Xiong <kxiong517@gmail.com>
2019-05-22 10:21:40 -0500

    updated server.js

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+// Console log in terminal what port is being listened to
+app.listen(port, () => {
+    console.log('listening to port:', port);
+})
+
+// Sending index.html to localhost:5000
+app.use(express.static('public'));
+
+// Sending quotes to localhost:5000/quotes
+app.get('/quotes', (req, res)=> {
+    let quotes_data = [
+        { text: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+        { text: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+        { text: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+    ];
+
+    res.send(quotes_data);
+})
+
